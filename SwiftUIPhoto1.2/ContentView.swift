44ca152e79b390a8becf3844a04d48cd760b9db3
jcastro27 <joeysk8ter@hotmail.com>
2022-06-24 11:18:56 -0500

    Initial Commit

---
+++ SwiftUIPhoto1.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPhoto1.2
+//
+//  Created by Joseph Castro on 6/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPhoto1.2/SwiftUIPhoto1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhoto1_2App.swift
+//  SwiftUIPhoto1.2
+//
+//  Created by Joseph Castro on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhoto1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
